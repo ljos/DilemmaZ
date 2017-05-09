b2b9a8882468fed3a8c765352c7c039f38d158f4
Eddiman <edbjorgen@gmail.com>
2017-05-09 19:27:49 +0200

    gui fix

--- forms/article.py
+++ forms/article.py
@@ -3,6 +3,16 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class Article(FlaskForm):
+
+    msg = """1. prevention of harm of owner/user
+2. prevention of harm to people
+3. prevention of harm to animal
+4. prevention of harm to property
+5. prevent harm by external causes
+6. Respect for autonomy
+7. Fidelity, truth telling"""
+
+
     title = StringField('Title', validators=[DataRequired()])
     authors = StringField('Authors', validators=[DataRequired()])
 
@@ -14,14 +24,13 @@ class Article(FlaskForm):
     logic = TextAreaField('Logic', validators=[])
 
 
-    feature = StringField('Feature', validators=[])
+    feature = TextAreaField('Feature', validators=[], default=msg)
 
-    actions = StringField('Actions', validators=[])
+    actions = TextAreaField('Actions', validators=[])
 
-    case = StringField('Case', validators=[])
+    case = TextAreaField('Case', validators=[])
 
-    duty_values = StringField('Duty Values', validators=[])
+    duty_values = TextAreaField('Duty Values', validators=[])
 
 
     submit = SubmitField("Submit")
-
