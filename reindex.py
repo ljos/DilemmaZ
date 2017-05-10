b59c9d1c413b57cf49a58c443f8ff11132de720d
Morten Linderud <morten@linderud.pw>
2017-05-10 12:41:01 +0200

    fixed reindex
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- reindex.py
+++ reindex.py
@@ -7,4 +7,15 @@ l = json.load(open("./articles"))
 es.indices.delete(index='dilemma', ignore=[400, 404])
 
 for i in l:
-    es.index(index='dilemma', doc_type='articles', body=i)
+
+    results = {}
+    results["title"] = i["title"]
+    results["authors"] = i["authors"]
+    results["dilemma_body"] = i["dilemma_body"]
+    results["article_url"] = i["article_url"]
+    results["logic"] = "" 
+    results["feature"] = ""
+    results["actions"] = ""
+    results["case"] = "" 
+    results["duty_values"] = ""
+    es.index(index='dilemma', doc_type='articles', body=results)
